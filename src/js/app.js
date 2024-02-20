ee7784b7faa2d0b02e1082c73d92f04a9c335402
vladislav-vov <128249554+vladislav-vov@users.noreply.github.com>
2024-02-20 18:23:19 +0200

    Главная страница: добавлена первая секция без адаптива

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,34 @@
 import isWebp from './helpers/isWebp.js';
+import calcScroll from './helpers/calcScroll.js';
 
 import '../scss/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
   isWebp();
+
+  const menuBtn = document.querySelector('.menu-btn');
+  const menuClose = document.querySelector('.menu__close');
+  const menu = document.querySelector('.menu');
+  const scroll = calcScroll();
+
+  function openMenu() {
+    menu.classList.add('menu--active');
+    document.body.style.overflowY = 'hidden';
+    document.body.style.marginRight = `${scroll}px`;
+  }
+
+  function closeMenu() {
+    menu.classList.remove('menu--active');
+    document.body.style.overflowY = 'scroll';
+    document.body.style.marginRight = '0';
+  }
+
+  menuBtn.addEventListener('click', openMenu);
+  menuClose.addEventListener('click', closeMenu);
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      closeMenu();
+    }
+  });
 });
